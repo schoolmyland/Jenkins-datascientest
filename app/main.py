78d50bc2aa42c28fb1336811ea0222879c416b18
PRIEZ Theo datascientest <priez.theo@live.fr>
2024-03-07 14:47:23 +0000

    we dchange

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
+from typing import Union
+from fastapi import FastAPI
+app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"Hello": "We Love Datascience, and we did it i think so. We build a CI/CD Pipeline !!"}
